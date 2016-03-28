85d385883aedb7395f05cd0d8c47a68b85f3a8e4
iKnowMagic <ilan@iknowmagic.net>
2016-03-28 09:29:08 -0700

    after homework

--- conFusion/www/js/controllers.js
+++ conFusion/www/js/controllers.js
@@ -179,6 +179,21 @@ angular.module('conFusion.controllers', [])
                         $scope.promotion = menuFactory.getPromotion().get({id:0});
       }])
 
+      .controller('AboutController', ['$scope', 'corporateFactory', 'baseURL', function($scope, corporateFactory, baseURL) {
+        $scope.baseURL = baseURL;
+        $scope.showLeaders = false;
+        $scope.message = 'Loading ...';
+        $scope.leaders = corporateFactory.query(
+          function(response) {
+            $scope.leaders = response;
+            $scope.showLeaders = true;
+          },
+          function(response) {
+            $scope.message = "Error: " + response.status + " " + response.statusText;
+          }
+        );
+      }])
+
 
 
 ;
